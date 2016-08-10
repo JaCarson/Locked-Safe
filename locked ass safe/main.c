b7eb9721df6a747f0f9dad491718d13b77d6174d
JaCarson <JohnAldenCarson@gmail.com>
2016-08-10 18:12:08 -0400

    Initial Commit

---
+++ locked ass safe/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  locked ass safe
+//
+//  Created by John Carson on 8/10/16.
+//  Copyright © 2016 John Carson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
